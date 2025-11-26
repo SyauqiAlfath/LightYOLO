d869a69b5a1066e8f4ce3976343ec79e45092d85
Syauqi Alfath <99640194+SyauqiAlfath@users.noreply.github.com>
2025-11-26 21:31:34 +0800

    Add try_1.py untuk mencoba model faster dan dataset cabai

---
+++ try_1.py
@@ -0,0 +1,22 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('ultralytics/cfg/models/v8/C2f-Faster-EMA.yaml')
+    # model.load('yolov8n.pt') # loading pretrain weights
+    model.train(data='data_cabai.yaml',
+                cache=False,
+                imgsz=640,
+                epochs=10,
+                batch=16,
+                close_mosaic=10,
+                workers=4,
+                device='0',
+                optimizer='SGD', # using SGD
+                # resume='', # last.pt path
+                # amp=False # close amp
+                # fraction=0.2,
+                project='runs/train',
+                name='exp',
+                )
